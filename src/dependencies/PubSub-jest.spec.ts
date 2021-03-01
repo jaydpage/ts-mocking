810ca116dda1269b0bb3fc4a796867fed3555569
Jayd Page <jayd.page@chillisoft.co.za>
2021-03-01 18:53:42 +0200

    Add remaining mocha tests

--- src/dependencies/PubSub-jest.spec.ts
+++ src/dependencies/PubSub-jest.spec.ts
@@ -1,4 +1,4 @@
-import { listenForCall } from '../mock-tests/jest_typed_mock'
+import { listenForCall } from '../tests/jest_typed_mock'
 import { PubSub } from './PubSub'
 
 describe('PubSub', () => {
